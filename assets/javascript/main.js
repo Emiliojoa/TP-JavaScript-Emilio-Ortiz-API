3f7fda0f97bda8c2e609e4b45a7e82248210a6aa
unknown <emiliojoaquinortizmalich@gmail.com>
2024-06-04 16:18:08 -0300

    se agrego la api

---
+++ assets/javascript/main.js
@@ -0,0 +1,44 @@
+const animeUrl = fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+
+
+const promes= async()=> {
+    try{
+        const respuesta=await animeUrl;
+        const data=await respuesta.json()
+        displayCharacters(data);
+        console.log(data)
+    }catch(error){
+    console.log("hubo un error")
+    }
+    
+}
+
+promes();
+
+// const promes2= async()=> {
+//     try{
+//         const respuesta=await animeUrl;
+//         const data=await respuesta.json()
+        
+//         return
+//     }catch(error){
+//     console.log("hubo un error")
+//     }
+// }
+
+function displayCharacters(anime) {
+
+    const container = document.getElementById("container");
+    
+    for(i in anime){
+      const animeElement = document.createElement("div");
+      animeElement.className = "anime";
+      animeElement.innerHTML = `
+            <img src="${anime[i].url}">
+            <h3>${anime[i].id}</h3>
+            
+        `;
+      container.appendChild(animeElement);
+    };
+}
+
